47b312437448e23e71889014c7ff0d1bc5689995
Ofer Shalev <ofershalev@gmail.com>
2020-01-24 03:03:49 -0800

    .

--- django/tutorial/quickstart/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# class User(admin.Model):
-#     first_name = admin.TextField()
-#     last_name = admin.TextField()
-#     pid = admin.TextField(unique=True, max_length=255)
-#     department = admin.TextField()
-#     gaga = admin.TextField()
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,19 +1,19 @@
 from werkzeug.utils import redirect
 
 from webapp import app
-from flask import render_template, url_for, json, request
+from flask import render_template, url_for, request
 from .mydb import *
 
 initdb()
-add_emp("firstA", "last", "depA", "2")
-add_emp("firstB", "last", "depA", "3")
-add_emp("firstC", "last", "depA", "4")
-add_emp("firstD", "last", "depA", "5")
-
-print("UPDATE: " + update_emp('108', 'S', 'F', 'A', '77').__str__())
-users = get_emps_all()
-for user in users:
-    print(user)
+# add_emp("firstA", "last", "depA", "2")
+# add_emp("firstB", "last", "depA", "3")
+# add_emp("firstC", "last", "depA", "4")
+# add_emp("firstD", "last", "depA", "5")
+#
+# print("UPDATE: " + update_emp('108', 'S', 'F', 'A', '77').__str__())
+# users = get_emps_all()
+# for user in users:
+#     print(user)
 
 
 
@@ -27,14 +27,34 @@ def delete(id):
     del_emp(id)
     return redirect(url_for('index'))
 
-@app.route('/user/<int:id>')
+
+
+@app.route('/edit/<int:id>')
 def edit(id):
-    emp = get_emp(id)
-    print(emp)
-    return render_template('user.html', emp=get_emp(id))
+    return render_template('edit.html', emp=get_emp(id)[0])
+
+@app.route('/editpost', methods=['GET', 'POST'])
+def editpost():
+    # print("UPDATE: " + update_emp('108', 'S', 'F', 'A', '77').__str__())
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    emp_no = request.form.get('emp_no')
+    department = request.form.get('department')
+    id = request.form.get('id')
+    print("Edit : " + first_name + last_name + id)
+    update_emp(id, first_name, last_name, department, emp_no)
+    return render_template('index.html', users=get_emps_all())
 
 @app.route('/add', methods=['GET', 'POST'])
-def signup():
-    first_name = request.form['first_name']
-    print("The email address is '" + first_name + "'")
-    return redirect(url_for('index'))
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods=['GET', 'POST'])
+def addpost():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    emp_no = request.form.get('emp_no')
+    department = request.form.get('department')
+    print("Adding : " + first_name + last_name)
+    add_emp(first_name, last_name, department, emp_no)
+    return render_template('index.html', users=get_emps_all())
